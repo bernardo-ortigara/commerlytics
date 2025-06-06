91a826676a2781ca5c9c7251cf4e7672be485027
dudzzpy <eduardoreiscosta2007@gmail.com>
2025-06-06 09:30:25 -0300

    Update valida.py

--- valida.py
+++ valida.py
@@ -1,1 +1,13 @@
 import defs
+
+def nome():
+  while true:
+    
+  
+cpf = input('CPF: ')
+data = input('Data: ')
+item = input('Item: ')
+valor = input('Valor: ')
+uantidade = input('Quantidade: ')
+comissionado = input('Comissionado: ')
+canal = input('Canal: ')
