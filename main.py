24415d946b838081e07797f606efd0771e314863
pedro-w-z <pwzwork08@gmail.com>
2025-06-07 17:34:29 -0300

    main.py de volta

---
+++ main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import defs
+import time
+
+while True:
+    defs.limpa_terminal()
+    escolha = defs.menu()
+
+    if escolha == '1':
+        defs.limpa_terminal()
+        defs.pesquisa()
+        print('')
+        defs.volta_menu()
+
+    elif escolha == '2':
+        defs.limpa_terminal()
+        defs.cadastra()
+
+    elif escolha == '3':
+        defs.limpa_terminal()
+        defs.lista()
+        print("\n\n\n")
+        defs.volta_menu()
+
+    elif escolha == '4':
+        defs.limpa_terminal()
+        defs.gera_relatorio()
+        print("\n\n\n")
+        defs.volta_menu()
+
+    elif escolha == '5':
+        defs.limpa_terminal()
+        defs.sai()
+        break
+
+    else:
+        defs.invalida()
+        time.sleep(2)
---
+++ valida.py
@@ -0,0 +1,52 @@
+import defs as defs
+
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> 6173199eae9546a64deda807f456881611f9df8d
+def nome():
+  while true:
+    nome = input('Nome: ')
+    while nome == '':
+      print('Erro! Entrada Vazia.')
+      nome = input('Digite um nome válido: ')
+    
+def cpf():
+  while true:
+  cpf = 1
+  cpf_digitado = input('Digite seu CPF (apenas números): ')
+  if cpf_digitado.isdigit(): #confere se tem pontos no meio 
+      if len(cpf_digitado) == 11: #confere se tem os 11 digitos
+          cpf = cpf_digitado #variavel que vai ser usada (cpf)
+      else:
+          while len(cpf_digitado) != 11:
+            cpf_digitado = input('Certifique-se que o CPF tenha 11 números. Digite novamente: ')
+            defs.tira_ponto_cpf()
+  else:
+      cpf_com_pontos = cpf_digitado
+      cpf_sem_pontos = cpf_com_pontos.replace('.', '')
+      cpf_digitado = cpf_sem_pontos
+    
+      if len(cpf_digitado) == 11:
+        cpf = cpf_sem_pontos
+      else:
+        while len(cpf_digitado) != 11:
+            cpf_digitado = input('Certifique-se que o CPF tenha 11 números. Digite novamente: ')
+            defs.tira_ponto_cpf()
+
+def data():
+  
+def item():
+  
+def valor():
+  
+def quantidade():
+  
+def comissionado():
+  
+def canal():
+<<<<<<< HEAD
+=======
+>>>>>>> 58f18a86dfe768a4682a43bf0a8c4a1e13a51881
+>>>>>>> 6173199eae9546a64deda807f456881611f9df8d
