afada5a6c8a1f6c0617a8c2b7adcdcd3d7cee967
Bernardo Ortigara <186219528+bernardo-ortigara@users.noreply.github.com>
2025-06-07 21:11:43 -0300

    Adicionar a função de relatórios

--- valida.py
+++ valida.py
@@ -1,26 +1,24 @@
-import defs
+import defs as defs
 
-<<<<<<< HEAD
-=======
 def nome():
-  while true:
+  while True:
     nome = input('Nome: ')
     while nome == '':
       print('Erro! Entrada Vazia.')
       nome = input('Digite um nome válido: ')
     
 def cpf():
-  while true:
-  cpf = 1
-  cpf_digitado = input('Digite seu CPF (apenas números): ')
-  if cpf_digitado.isdigit(): #confere se tem pontos no meio 
+  while True:
+    cpf = 1
+    cpf_digitado = input('Digite seu CPF (apenas números): ')
+    if cpf_digitado.isdigit(): #confere se tem pontos no meio 
       if len(cpf_digitado) == 11: #confere se tem os 11 digitos
           cpf = cpf_digitado #variavel que vai ser usada (cpf)
       else:
           while len(cpf_digitado) != 11:
             cpf_digitado = input('Certifique-se que o CPF tenha 11 números. Digite novamente: ')
             defs.tira_ponto_cpf()
-  else:
+    else:
       cpf_com_pontos = cpf_digitado
       cpf_sem_pontos = cpf_com_pontos.replace('.', '')
       cpf_digitado = cpf_sem_pontos
@@ -32,15 +30,15 @@ def cpf():
             cpf_digitado = input('Certifique-se que o CPF tenha 11 números. Digite novamente: ')
             defs.tira_ponto_cpf()
 
-def data():
+#def data():
   
-def item():
+#def item():
   
-def valor():
+#def valor():
   
-def quantidade():
+#def quantidade():
   
-def comissionado():
+#def comissionado():
   
-def canal():
->>>>>>> 58f18a86dfe768a4682a43bf0a8c4a1e13a51881
+#def canal():
+
