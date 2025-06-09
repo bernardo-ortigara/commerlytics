1726f669ce94a83f46540cfe7e20f22fcfb8b5dd
dudzzpy <eduardoreiscosta2007@gmail.com>
2025-06-09 13:55:53 -0300

    nome, cpf, data, valor finalizados

--- valida.py
+++ valida.py
@@ -30,16 +30,45 @@ def cpf():
             cpf_digitado = input('Certifique-se que o CPF tenha 11 números. Digite novamente: ')
             defs.tira_ponto_cpf()
 
-#def data():
-  
+def data():
+  while True:
+    data = input('Data (dd/mm/aaaa): ')
+    if data == '':
+        print('Erro! Entrada inválida.')
+        continue
+    temp = ''.join(data.split('/'))  # retorna uma string de valores sem '/'
+    if not temp.isnumeric():         # analisa se essa string tem caracteres
+        print('Insira uma data válida')
+        continue
+    # data.count('/') retorna o numero de '/' na data
+    if data.count('/') == 2 and data != '//':  # Checa se data tem duas '/' e não é apenas //
+        dia, mes, ano = data.split('/')        # cada valor divido é jogado nas variaveis em sequencia
+        if 1 <= int(dia) <= 31 and 1 <= int(mes) <= 12 and 1900 <= int(ano) <= 2025:
+            return data.strip(' ')
+        else:
+            print('Dia/Mes/Ano Inválido(s)')
+    else:
+        print('A data deve seguir o padrão dd/mm/aaaa')
+    
 #def item():
   
-#def valor():
-  
+def valor():
+  while True:
+    entrada = input('Valor: ')
+    
+    if entrada == '':
+        print('Erro! Entrada inválida.')
+    elif entrada.replace('.', '', 1).isdigit():
+        valor = float(entrada)
+        if valor < 0:
+            print('Erro! Valor não pode ser negativo.')
+        else:
+            break
+    else:
+        print('Erro! Digite um número válido.')
+        
 #def quantidade():
   
 #def comissionado():
   
-#def canal():
-
 #def canal():
